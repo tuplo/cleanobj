16c53ac5f5e3c3b36d7f4c93c190298fc1a9b3d9
Rui Costa <ruicosta042@gmail.com>
2022-07-15 12:51:09 +0100

    feat: cleanobj

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,47 @@
-export function main() {
-	console.log('Hello world!');
+type Obj = Record<string, unknown>;
+
+type Options = {
+	ignore?: (key: string, value: unknown) => boolean;
+};
+
+export default function cleanobj<T extends Obj>(obj: T, options?: Options): T {
+	const { ignore = () => false } = options || {};
+
+	const ret = {} as Obj;
+	for (const key of Object.keys(obj)) {
+		const value = obj[key];
+
+		if (ignore(key, value)) {
+			ret[key] = obj[key];
+			continue;
+		}
+
+		if (typeof value === 'undefined') continue;
+
+		if (typeof value === 'function') continue;
+
+		if (typeof value === 'object' && !Array.isArray(value) && value !== null) {
+			const n = cleanobj(value as Obj);
+			if (Object.keys(n).length > 0) ret[key] = n;
+			continue;
+		}
+
+		if (typeof value === 'string') {
+			const s = value.replace(/\s+/g, ' ').trim();
+			if (s.length > 0) ret[key] = s;
+			continue;
+		}
+
+		if (Array.isArray(value)) {
+			const o = Object.assign({} as Obj, value);
+			const oc = cleanobj(o);
+			const n = Object.values(oc);
+			if (n.length > 0) ret[key] = n;
+			continue;
+		}
+
+		ret[key] = obj[key];
+	}
+
+	return ret as T;
 }
